bc8e9815f505ae1b37e02153826c8ebc626ef131
Diego <Diego@DESKTOP-0LAR25P>
2024-04-04 21:35:21 -0500

    Agregar archivos de proyecto.

---
+++ dvillavicencioT1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace dvillavicencioT1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ dvillavicencioT1/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace dvillavicencioT1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculo_Clicked(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            int edad = Convert.ToInt32(txtEdad.Text);
+            int salario = Convert.ToInt32(txtSalario.Text);
+
+
+            int aporte = (int)(salario * 0.0945);
+
+            DisplayAlert("Alerta", "Bienvenido: " + nombre + apellido +"\nTienes " + edad + "años"+ "\nTu aporte mensual es de: " + aporte, "Cerrar");
+
+        }
+    }
+
+}
